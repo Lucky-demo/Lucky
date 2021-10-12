da2ac7cf85a9283859373d597076f89466db7e89
Salvador <Sagarcia2713@gmail.com>
2021-10-12 17:33:41 -0500

    Initial Commit

---
+++ Luck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Luck
+//
+//  Created by Salvador Garcia on 10/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
